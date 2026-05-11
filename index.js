bd3ddbc081d475850e5f147a6ad7278e0271514f
Himanshup0703 <patelpranya15@gmail.com>
2026-05-11 21:05:08 +0530

    connected local repo

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log("hahaha")
+
+let btn=document.querySelector(".two");
+let body=document.querySelector("body");
+
+btn.addEventListener("click",function(){
+    // console.log("hiee");
+    body.style.backgroundColor="lightyellow"
+
+})
